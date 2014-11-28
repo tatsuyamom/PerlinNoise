23a643838a728da396a06252659f8c8df6511092
t_hiroi <t_hiroi@BH-0000096.local>
2014-11-28 14:47:44 +0900

    remove bin

--- xcode/build/ParticleNoise.build/Debug/ParticleNoise.build/Objects-normal/i386/Particle.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/t_hiroi/Documents/Cinder/cinder_0.8.5_mac/myApps/ParticleNoise/xcode/Particle.cpp \
-  /Users/t_hiroi/Documents/Cinder/cinder_0.8.5_mac/myApps/ParticleNoise/xcode/Particle.h
